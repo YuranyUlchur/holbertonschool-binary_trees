00e67faeb786891928de61a3ca175ce98269b47f
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-16 18:02:15 -0500

    add function binary_tree_preorder

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - function that checks if a given node is a root
+ * @tree: tree pointer structure
+ * @func: function (print)
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
+}
