8e930022983606007dd68b42fac72e164210f067
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-16 17:47:39 -0500

    add condition the function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-	if (!node->left && !node->right)
-		return (1);
-	return (0);
+	if (!node || node->left || node->right)
+		return (0);
+	return (1);
 }
