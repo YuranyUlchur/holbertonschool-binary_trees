cfb15e967044b2b22ee78ebee6e5f2b0c77019d7
YuranyUlchur <ulchuryurani80@gmail.com>
2023-01-19 14:56:46 -0500

    add function balance binary tree
    Co-authored-by: felipevcc <felipevillamizarc@gmail.com>

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,40 +7,34 @@
  */
 size_t _height(const binary_tree_t *tree)
 {
-	size_t height_l;
-	size_t height_r;
+	size_t height_l, height_r;
 
-	height_l = tree->left ? 1 + _height(tree->left) : 0;
-	height_r = tree->right ? 1 + _height(tree->right) : 0;
-	return (height_l > height_r ? height_l : height_r);
+	if (!tree)
+		return (0);
+
+	height_l = _height(tree->left);
+	height_r = _height(tree->right);
+
+	if (height_l >= height_r)
+		return (height_l + 1);
+
+	return (height_r + 1);
 }
-
 /**
- * binary_tree_nodes - function that measures the balance
+ * binary_tree_balance -function that measures the balance
  * @tree: Pointer binary_tree_t
  * Return: integer
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int balance1, balance2;
+	int balance1 = 0, balance2 = 0;
 
-	if (tree == NULL)
+	if (!tree)
 		return (0);
 
-	balance1 = _height(tree->left);
-	if (balance1 == -1)
-	{
-		return (-1);
-	}
-
-	balance2 = _height(tree->right);
-	if (balance2 == -1)
-	{
-		return (-1);
-	}
-	if (abs(balance1 - balance2) > 1)
-	{
-		return (-1);
-	}
-	return ((balance1 - balance2) + 1);
+	if (tree->left)
+		balance1 = _height(tree->left);
+	if (tree->right)
+		balance2 = _height(tree->right);
+	return (balance1 - balance2);
 }
